125622330e62730addec4931b0db708a50c34ee2
Kgrau1 <kgrau@madisoncollege.edu>
2024-02-12 20:37:17 -0600

    Added the start of code to Attendance and updated pom file for needed dependencies

--- src/main/java/entity/Attendance.java
+++ src/main/java/entity/Attendance.java
@@ -1,4 +1,23 @@
 package entity;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import javax.persistence.*;
+@Entity(name="Attendance")
+@Table(name="attendance")
 public class Attendance {
+    @Id
+    private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
