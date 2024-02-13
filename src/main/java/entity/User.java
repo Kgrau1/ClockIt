61e4d278893b1167b3db787cbbe2799b93e23112
Kgrau1 <kgrau@madisoncollege.edu>
2024-02-13 17:21:49 -0600

    Added getters, setters, and javadocs to User

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -8,18 +8,101 @@ import javax.persistence.Id;
 import java.time.LocalDateTime;
 import javax.persistence.*;
 
+/**
+ * The type User.
+ */
 @Entity(name="User")
 @Table(name="users")
 public class User {
 
     @Id
     private int id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password_hash")
+    private String passwordHash;
 
+
+    /**
+     * Instantiates a new User.
+     */
+    public User() {
+    }
+
+    /**
+     * Instantiates a new User.
+     *
+     * @param id           the id
+     * @param username     the username
+     * @param passwordHash the password hash
+     */
+    public User(int id, String username, String passwordHash) {
+        this.id = id;
+        this.username = username;
+        this.passwordHash = passwordHash;
+    }
+
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
+    }
+
+    /**
+     * Gets username.
+     *
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Sets username.
+     *
+     * @param username the username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * Gets password hash.
+     *
+     * @return the password hash
+     */
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    /**
+     * Sets password hash.
+     *
+     * @param passwordHash the password hash
+     */
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", passwordHash='" + passwordHash + '\'' +
+                '}';
     }
 }
