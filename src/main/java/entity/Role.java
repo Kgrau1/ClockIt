80b6bdbf3517f83f6654ef9c2f4c213e7c0750dd
Kgrau1 <kgrau@madisoncollege.edu>
2024-02-13 17:12:31 -0600

    Added getters, setters, and javadocs to Role

--- src/main/java/entity/Role.java
+++ src/main/java/entity/Role.java
@@ -8,18 +8,76 @@ import javax.persistence.Id;
 import java.time.LocalDateTime;
 import javax.persistence.*;
 
+/**
+ * The type Role.
+ */
 @Entity(name="Role")
 @Table(name="roles")
 public class Role {
 
     @Id
     private int id;
+    @Column(name = "role_name")
+    private String roleName;
 
+    /**
+     * Instantiates a new Role.
+     */
+    public Role() {
+    }
+
+    /**
+     * Instantiates a new Role.
+     *
+     * @param id       the id
+     * @param roleName the role name
+     */
+    public Role(int id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    /**
+     * Gets role name.
+     *
+     * @return the role name
+     */
+    public String getRoleName() {
+        return roleName;
+    }
+
+    /**
+     * Sets role name.
+     *
+     * @param roleName the role name
+     */
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                '}';
     }
 }
